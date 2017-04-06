760e710749e75ab95e6448c6c2cd9b49711ee28f
Hivison do Nascimento Moura <hivison.moura@gmail.com>
2017-04-05 22:14:52 -0300

    Create a flow to update and fix the sidibar menu.

--- src/main/java/com/algaworks/vinhos/controller/VinhosController.java
+++ src/main/java/com/algaworks/vinhos/controller/VinhosController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -28,6 +29,7 @@ public class VinhosController {
 	@GetMapping("/novo")
 	public ModelAndView novo(Vinho vinho){
 		ModelAndView mv = new ModelAndView("vinho/cadastro-vinho");
+		mv.addObject(vinho);
 		mv.addObject("tipos", TipoVinho.values());
 		return mv;
 	}
@@ -50,6 +52,12 @@ public class VinhosController {
 		modelAndView.addObject("vinhos", vinhos.findByNomeContainingIgnoreCase(
 				Optional.ofNullable(vinhoFilter.getNome()).orElse("%")));
 		return  modelAndView;
+	}
+
+	@GetMapping("/{codigo}")
+	public ModelAndView editar(@PathVariable Long codigo){
+		Vinho vinho = vinhos.findOne(codigo);
+		return novo(vinho);
 	}
 	
 }
